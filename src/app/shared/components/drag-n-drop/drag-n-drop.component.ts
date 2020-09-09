691e5659e1dda560fed1a1cf165c9b681e07a8d1
Yaroslav_Zdebskyy <zdebskyyslavik>
2020-09-09 17:49:23 +0300

    finished

--- src/app/shared/components/drag-n-drop/drag-n-drop.component.ts
+++ src/app/shared/components/drag-n-drop/drag-n-drop.component.ts
@@ -1,45 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import {CdkDragDrop, moveItemInArray, Point} from '@angular/cdk/drag-drop';
+import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
+import {CdkDragDrop} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-drag-n-drop',
   templateUrl: './drag-n-drop.component.html',
-  styleUrls: ['./drag-n-drop.component.scss']
+  styleUrls: ['./drag-n-drop.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class DragNDropComponent {
+  public mutatedEvents: string[];
 
-  public ivents: string[];
+  private originalEvents: string[];
+  private currentEvent: string;
 
   constructor() {
-    this.ivents = [
+    this.originalEvents = [
       'Section',
       'Instructions',
       'Criteria',
       'Questions',
     ];
+    this.mutatedEvents = [...this.originalEvents];
+    this.currentEvent = '';
   }
 
-
-
-  // public dropped(event): void {
-  //
-  // }
-
-  public dropped(event: CdkDragDrop<string[]>): void {
-    console.log('dropped => ', event);
-
-  }
-public drop(event): void {
-    console.log('drop => ', event);
-    moveItemInArray(
-    this.ivents,
-    event.previousIndex,
-    event.previousIndex
-  );
+  public get isShowAdditionalItem(): boolean {
+    return !Boolean(this.currentEvent);
   }
 
-  public position(a, b): Point {
-    return {x: 0, y: 0};
+  public get draggableTxt(): string {
+    return this.isShowAdditionalItem
+      ? 'Move it to event'
+      : `Add <span class="cur-event"> ${this.currentEvent} </span> to your Plan to provide guidance to your Interviewers`;
   }
 
+  public isDragDisabled(index: number): boolean {
+    return this.originalEvents[index] !== this.currentEvent;
+  }
+
+  public onDrop({ currentIndex }: CdkDragDrop<string[]>): void {
+    this.currentEvent = this.originalEvents[currentIndex];
+    this.mutatedEvents = this.originalEvents.map(((item: string, index: number) => index === currentIndex ? this.draggableTxt : item));
+  }
 }
